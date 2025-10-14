c94ba9b6c192d3ce4a11129b0c49bf7ad95b02c6
gujunGwon <css98321@naver.com>
2025-10-14 23:53:51 +0900

    to prob9

---
+++ chap6/prob8/symlink.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+int main(int argc, char *argv[]){
+	if(symlink(argv[1], argv[2]) == -1){
+		perror("symlink");
+		exit(1);
+	}
+
+	exit(0);
+}
