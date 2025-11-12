f62af2717ba88988c141b6cb39cc53cee2647882
gujunGwon <css98321@naver.com>
2025-11-13 03:02:39 +0900

    to midterm 2.(3)

--- midterm/bookquery.c
+++ midterm/bookquery.c
@@ -27,16 +27,27 @@ int main(int argc, char *argv[]){
 
 		lseek(fd, 0, SEEK_SET);
 		
+		if(queryFlag != 0 && queryFlag != 1) {
+                        printf("Please enter \"1\" or \"0\"\n\n");
+                        continue;
+                }
+
 		printf("%4s %12s %12s %15s %18s %11s\n", "id", "bookname", "author", "year", "numofborrow", "borrow");
-		while(read(fd, (char *)&record, sizeof(record)) > 0){
-			if(queryFlag == 1){
-				printf("%4d %12s %12s %15d %18d %11d\n", record.id, record.name, record.author, record.year, record.numofborrow, record.borrow);	
+		
+		while(read(fd, (char *)&record, sizeof(record)) > 0){ //printing book list depending on queryFlag
+			if(queryFlag == 0){
+				printf("%4d %12s %12s %15d %18d", record.id, record.name, record.author, record.year, record.numofborrow);
+				if(record.borrow == 1) printf("       True\n");
+				else if(record.borrow == 0) printf("      False\n");	
 			}
 
-			else if(queryFlag == 0 && record.borrow == 1){
-				printf("%4d %12s %12s %15d %18d %11d\n", record.id, record.name, record.author, record.year, record.numofborrow, record.borrow);
+			else if(queryFlag == 1 && record.borrow == 1){
+				printf("%4d %12s %12s %15d %18d       True\n", record.id, record.name, record.author, record.year, record.numofborrow);
 			}
 		}
-	
+
+		printf("\n");	
 	} while (1);
+
+	close(fd);
 }
